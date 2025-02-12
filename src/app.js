2d9106ed46598e7fca18d8ae143970a3e359743c
saikumarsApp <saikumarnp8@gmail.com>
2025-02-12 10:14:29 +0530

    exploring API's

--- src/app.js
+++ src/app.js
@@ -2,6 +2,13 @@ const express = require("express");
 
 const app = express();
 
+app.get("/user", (req, res) => {
+    res.send({firstname: "Sai Kumar", lastname: "Bhukya"});
+})
+app.use("/hello", (req, res) => {
+    res.send("Hello Welcome!");
+})
+
 app.listen(3000, () => {
     console.log("Server  is running");
 })
